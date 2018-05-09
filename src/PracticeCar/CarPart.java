770a534289bb3947586351498352313222b166e6
Alexander Hartson <alexander-hartson@live.com>
2018-05-09 10:42:59 -0500

    adding car part class

--- src/PracticeCar/CarPart.java
+++ src/PracticeCar/CarPart.java
@@ -1,4 +1,18 @@
 package PracticeCar;
 
 public class CarPart {
+
+    private String type;
+
+    public CarPart(String t) {
+        type = t;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
